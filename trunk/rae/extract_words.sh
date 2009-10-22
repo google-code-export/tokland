c5225878222cb45c585cf36cdd225111dc5838f8
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2009-10-22 19:47:38 +0000

    prettify

--- trunk/rae/extract_words.sh
+++ trunk/rae/extract_words.sh
@@ -4,11 +4,13 @@ set -e
 debug() { echo "$@" >&2; }
 
 extract_words_from_binary() {
-  grep -a -o "[a-zA-Záéíóúñ \.,]\+" "$FILE" | \
-    grep "[a-zA-Záéíóúñ]" | \
-    grep -v "^[[:space:]]" | \
-    sed "s/\.$//; s/,.*$//"
+  grep -a -o "[a-zA-Záéíóúñ \.,]\+" |
+    grep "[a-zA-Záéíóúñ]" |
+    grep -v "^[[:space:]]" |
+    sed "s/\.$//; s/,.*$//" |
+    grep -v "^[[:space:]]*$" |
+    sort -u
 }            
            
-# cdrom-rae2.2/data/ixLm/_3son.fdt
+# source: cdrom-rae2.2/data/ixLm/_3son.fdt
 cat _3son.fdt | extract_words_from_binary > words.txt
