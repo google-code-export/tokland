0fc7c338c2e6870ae23c044c578c449197b5244e
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2009-10-22 19:48:03 +0000

    create function

--- rae/process_html.rb
+++ rae/process_html.rb
@@ -2,26 +2,22 @@
 require 'rubygems'
 require 'nokogiri'
 
-#if ARGV.size != 2
-#  STDERR.write("Usage: process_html source_html_directory output_directory\n")
-#  exit 1
-#end
-#indir, outdir = ARGV
-
-indir, outdir = "html", "html2"
-FileUtils.mkdir_p(outdir)
-
-Dir.glob(File.join(indir, "*.html")).sort.each do |path|
-  doc = Nokogiri::HTML(open(path))  
-  unless contents = doc.search("div#definicion div:last").first
-    STDERR.write("Definition not found: #{path}\n")
-    next
-  end
-  contents.search("a").each do |a|
-    if a.attributes["href"].to_s =~ /origen=RAE&LEMA=(.*?)&/
-      a.set_attribute("href", $1+".html") 
+def process_html(indir, outdir)
+  FileUtils.mkdir_p(outdir)
+  Dir.glob(File.join(indir, "*.html")).sort.each do |path|
+    doc = Nokogiri::HTML(open(path))  
+    unless contents = doc.search("div#definicion div:last").first
+      STDERR.write("Definition not found: #{path}\n")
+      next
     end
+    contents.search("a").each do |a|
+      if a.attributes["href"].to_s =~ /origen=RAE&LEMA=(.*?)&/
+        a.set_attribute("href", $1+".html") 
+      end
+    end
+    output_path = File.join(outdir, File.basename(path))
+    open(output_path, "w") { |f| f.write(contents.to_s) }
   end
-  output_path = File.join(outdir, File.basename(path))
-  open(output_path, "w") { |f| f.write(contents) }
 end
+
+process_html("html", "html2")
