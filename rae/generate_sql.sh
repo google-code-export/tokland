3e64c657183e398e72458bcb3fdd545947f73448
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2009-10-31 09:03:17 +0000

    minor

--- rae/generate_sql.sh
+++ rae/generate_sql.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
-TABLENAME="words"
-NAME="drae2.2"
+
+debug() { echo "$@" >&2; }
+escape_sql() { sed "s/'/''/g"; }
 
 generate_sql() {
-  TABLE=$1
-  DIRECTORY=$2  
+  local TABLE=$1
+  local DIRECTORY=$2  
   
-  echo "CREATE TABLE $TABLE(word varchar(255), definition text);"
+  echo "CREATE TABLE $TABLE(word varchar(255) PRIMARY KEY, definition text);"
   find $DIRECTORY -type f -name '*.html' | while read FILE; do
     WORD=$(basename "$FILE" ".html")
-    DEFINITION=$(cat "$FILE" | sed "s/'/''/g")
+    DEFINITION=$(cat "$FILE" | escape_sql)
     echo "INSERT INTO $TABLE (word, definition) VALUES ('$WORD', '$DEFINITION');"
   done
 }
 
-generate_sql "$TABLE" "$NAME" | gzip > "$NAME.sql.gz"
+generate_sql "words" "drae2.2" | gzip > "drae2.2.sql.gz"
+
+# Create a sqlite3 database:
+# generate_sql "words" "drae2.2"| sqlite3 database.sqlite3
