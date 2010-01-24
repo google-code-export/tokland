e3c9369fb89b9789d548b46ee21e96f440b3f47d
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-01-24 10:25:40 +0000

    not all divs but the first one

--- trunk/rae/process_html.rb
+++ trunk/rae/process_html.rb
@@ -2,21 +2,29 @@
 require 'rubygems'
 require 'nokogiri'
 
-def process_html(indir, outdir)
-  FileUtils.mkdir_p(outdir)
-  Dir.glob(File.join(indir, "*.html")).sort.map do |path|
-    doc = Nokogiri::HTML(open(path))  
-    unless contents = doc.search("div#definicion div:last").first
-      STDERR.write("html does not contain definition: #{path}\n")
-      next
-    end
+def process_html(data)
+  doc = Nokogiri::HTML(data)
+  divs = doc.search("div#definicion div")
+  if divs.empty?
+    STDERR.write("html does not contain definition: #{path}\n")
+    return
+  end
+  divs[1..-1].map do |contents|
     contents.search("a").each do |a|
       if a.attributes["href"].to_s =~ /origen=RAE&LEMA=(.*?)&/
         a.set_attribute("href", $1+".html") 
       end
     end
+    contents.inner_html
+  end.join
+end
+
+def process_html_dir(indir, outdir)
+  FileUtils.mkdir_p(outdir)
+  Dir.glob(File.join(indir, "*.html")).sort.map do |path|
+    outdata = process_html(open(path))
     output_path = File.join(outdir, File.basename(path))
-    open(output_path, "w") { |f| f.write(contents.to_s) }
+    open(output_path, "w") { |f| f.write(outdata) }
     output_path
   end.size
 end
