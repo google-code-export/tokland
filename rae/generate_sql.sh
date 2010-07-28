2409ed0e467aca0a06dd29052a213e340b579666
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-07-28 22:00:40 +0000

    sql: remove primary key and create index

--- rae/generate_sql.sh
+++ rae/generate_sql.sh
@@ -2,21 +2,21 @@
 set -e
 
 debug() { echo "$@" >&2; }
-escape_sql() { sed "s/'/''/g"; }
+escape_sql() { sed "s/'/''/g; s/^[[:space:]]*//; s/[[:space:]]*$//"; }
 
 generate_sql() {
   local TABLE=$1
   local DIRECTORY=$2  
   
-  echo "CREATE TABLE $TABLE(word varchar(255) PRIMARY KEY, definition text);"
+  echo "CREATE TABLE $TABLE(word varchar(255), html text, text text);"
   find $DIRECTORY -type f -name '*.html' | while read FILE; do
     WORD=$(basename "$FILE" ".html")
-    DEFINITION=$(cat "$FILE" | escape_sql)
-    echo "INSERT INTO $TABLE (word, definition) VALUES ('$WORD', '$DEFINITION');"
+    HTML=$(< "$FILE" escape_sql)
+    TEXT=$(lynx -nolist -dump "$FILE" | recode iso8859-1..utf8 | tail -n+7 | head -n-1 | escape_sql)
+    echo "INSERT INTO $TABLE (word, html, text) VALUES ('$WORD', '$HTML', '$TEXT');"
   done
+  echo "CREATE INDEX word_index ON words (word);"
 }
 
-generate_sql "words" "drae2.2" | gzip > "drae2.2.sql.gz"
-
-# Create a sqlite3 database:
-# generate_sql "words" "drae2.2"| sqlite3 database.sqlite3
+generate_sql "words" "html2" #| gzip > "drae-2009.sql.gz"
+# tee | sqlite3 drae-2009.sqlite3 |
