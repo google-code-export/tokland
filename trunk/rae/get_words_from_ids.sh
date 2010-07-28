67202beab20ef1d3241a868a6ea4611c98342735
tokland <tokland@081718a6-6b25-0410-9b06-e1944711deb8>
2010-07-28 22:16:34 +0000

    use collate utf8_bin

--- trunk/rae/get_words_from_ids.sh
+++ trunk/rae/get_words_from_ids.sh
@@ -4,7 +4,8 @@ set -e
 extract() {
   local DIR=$1
   grep -o '<span class="eLema"><B>[^<]*</B>' $DIR/*.html | 
-    cut -d">" -f3 | cut -d"<" -f1
+    cut -d">" -f3 | cut -d"<" -f1 | 
+    sed "s/\xe2\x80\x92\xcc\x81/-/g; s/^-//g; s/-$//g" 
 }
 
-extract "html" > words.txt
+extract "html" | sort -u > words.txt
